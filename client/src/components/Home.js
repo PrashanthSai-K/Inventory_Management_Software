328912b67069b822dd2fd38dc4a00044adbd3cba
Hari-haran-p <harichris28@gmail.com>
2023-07-20 16:43:21 +0530

    design updated

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,36 +1,35 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
 
 function Home() {
-    const [open, setOpen] = useState(true);
-    const Menus = [
-      { title: "Dashboard", src: "Chart_fill" },
-      { title: "Inbox", src: "Chat" },
-      { title: "Accounts", src: "User", gap: true },
-      { title: "Schedule ", src: "Calendar" },
-      { title: "Search", src: "Search" },
-      { title: "Analytics", src: "Chart" },
-      { title: "Files ", src: "Folder", gap: true },
-      { title: "Setting", src: "Setting" },
-    ];
-  
-   
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "Accounts", src: "User" },
+    { title: "Schedule ", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files ", src: "Folder" },
+    { title: "Setting", src: "Setting" }, 
+  ];
+
   return (
-    <div className="flex" >
-      <div style = {{backgroundColor:"darkblue"}}
+    <div className="flex">
+      <div
+        style={{ backgroundColor: "darkblue" }}
         className={` ${
           open ? "w-72" : "w-20 "
         } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
       >
         <img
-          src="/images/control.png"
+          src="/images/control.png" alt=""
           className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
            border-2 rounded-full  ${!open && "rotate-180"}`}
           onClick={() => setOpen(!open)}
         />
         <div className="flex gap-x-4 items-center">
           <img
-            src="/images/logo.png"
+            src="/images/logo.png" alt=""
             className={`cursor-pointer duration-500 ${
               open && "rotate-[360deg]"
             }`}
@@ -47,12 +46,12 @@ function Home() {
           {Menus.map((Menu, index) => (
             <li
               key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 hover:bg-sky-700 
               ${Menu.gap ? "mt-9" : "mt-2"} ${
                 index === 0 && "bg-light-white"
               } `}
             >
-              <img src={`/images/${Menu.src}.png`} />
+              <img src={`/images/${Menu.src}.png`} alt=""/ >
               <span className={`${!open && "hidden"} origin-left duration-200`}>
                 {Menu.title}
               </span>
@@ -61,11 +60,22 @@ function Home() {
         </ul>
       </div>
       <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">Home Page</h1>
+        <h1 className="text-2xl font-semibold underline">Home</h1>
+        <div className="flex h-full justify-around mt-8">
+        <div className="h-1/4 w-1/4 bg-emerald-400 rounded-3xl flex wrap">
+          <div className="ml-10 items-center flex-col justify-around w-7/12 flex">
+          <div className="text-xl font-Poppins font-semibold ">Total value</div>
+          <div className="text-xl">20 lacks</div>
+          <div className="text-xl">view</div>
+          </div>
+          <img src="./images/first.png" alt="" className="justify-items-end	w-1/2 h-full pr-2"></img>
+        </div>
+        <div className="h-1/4 w-1/4 bg-yellow-200 rounded-3xl">hello</div>
+        <div className="h-1/4 w-1/4 bg-fuchsia-400 rounded-3xl">hello</div>
+        </div>
       </div>
     </div>
   );
-  
 }
 
-export default Home
+export default Home;
