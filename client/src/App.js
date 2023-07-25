725a59843d688fcacd288fd38b470bec297d2775
PrashanthSai-K <114280375+PrashanthSai-K@users.noreply.github.com>
2023-07-25 17:14:52 +0530

    entry files updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
+import {Routes , Route} from "react-router-dom";
+import ManufacturerEntry from './components/ManufacturerEntry';
+import SupplierEntry from './components/SupplierEntry';
+
 
 function App() {
   return (
-    <Home />
+    <>
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/manufactureradd' element={<ManufacturerEntry />} />
+    <Route path='/supplieradd' element={<SupplierEntry />} />
+    </Routes>
+    </>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,17 +1,50 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql"); 
+const dotenv = require('dotenv').config();
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+const conn = mysql.createConnection({
+    host:process.env.DB_HOST_LOCAL,
+    user:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT_LOCAL
+})
+
+
+conn.connect(()=>{
+    console.log("Connected to DB sucessfully")
+})
 
 app.get("/",(req,res)=>{
     res.send("Hello from backend")
 })
 
+app.post("/manufactureradd", (req,res)=>{
+    const name = req.body.name
+    conn.query("INSERT INTO manufacturer (name) VALUES (?)",[name],(error,result)=>{
+        if(error) console.log(error)
+    })
+})
+
+app.post("/supplieradd", (req,res)=>{
+
+    const name = req.body.name;
+    const address = req.body.address;
+    const contact = req.body.contact;
+    conn.query("INSERT INTO supplier (name,address,contact) VALUES (?,?,?)",[name,address,contact],(error,result)=>{
+        if(error) console.log(error)
+        else console.log(result);
+        
+    })
+})
+
 
 app.listen(4000, ()=>console.log("App listening on port 4000"));
-
